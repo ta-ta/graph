1be6782cee20f1ff350f730b7ef1d9c3e9f424db
mzm <mozumikuon@mzm.local>
2019-11-18 22:49:23 +0900

    コマンドライン引数に一部対応（ファイル、重み、有向/無向）

--- graph_draw.py
+++ graph_draw.py
@@ -1,53 +1,110 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import sys
 
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from PIL import Image
+
+WEIGHTED = False
 
 # グラフ描画
 def graph_draw(graph: nx.MultiDiGraph):
-    pos = nx.spring_layout(graph, k=0.7)
-
-    # 重みを数値だけにする
-    for from_, to_ , weight_ in graph.edges(data=True):
-        # 重みがない時は表示しない
-        if weight_.get('weight') != None:
-            edge_labels = {(from_, to_): weight_['weight']}
-            nx.draw_networkx_edge_labels(graph, pos, edge_labels = edge_labels)
-        else:
-            nx.draw_networkx_edges(graph, pos)
+    pos = nx.spring_layout(graph)
 
     nx.draw_networkx(graph, pos)
 
-    # 辺が重ならないようにしたり、枠をなくしたり
+    ag = nx.nx_agraph.to_agraph(graph)
+    ag.layout()
+    ag.draw("graph.png")
+
+    # 画像の読み込み
+    image = Image.open("graph.png")
+    image_list = np.asarray(image)
+
+    plt.imshow(image_list)
     plt.tight_layout()
     plt.axis("off")
     plt.show()
 
 if __name__ == '__main__':
-    mdgraph = nx.MultiDiGraph()
-    num = int(input())
+    parser = argparse.ArgumentParser()
 
-    for i in range(num):
-        edge_info = input().split(" ")
-        from_ = int(edge_info[0])
-        to_ = int(edge_info[1])
-        weight_ = 0
+    parser.add_argument('--file', help='problem file', type=str)
+    parser.add_argument('-w', '--weighted', help='Existence of weight(def:None)', action='store_true')
+    parser.add_argument('-f', '--format', help='Input format is edges or matrix(def:edges)', default='edges', type=str)
+    parser.add_argument('-i', '--indexed', help='0-indexed or 1-indexed(def:1)', default=1, type=int)
+    parser.add_argument('-d', '--directed', help='Directed or UnDirected(def:Undirected)', action='store_true')
+    parser.add_argument('-t', '--index_transform', help='1-indexed transform 0-indexed(def:No)', action='store_true')
 
-        # 入力の確認
-        if len(edge_info) != 3 and len(edge_info) != 2:
-            print("Args Error")
-            sys.exit()
-        elif len(edge_info) == 3 :
-            weight_ = int(edge_info[2])
+    args = parser.parse_args()
+    problem_file = args.file
 
-        # 重み付きの辺を追加
-        if weight_ != 0:
-            mdgraph.add_weighted_edges_from([(from_, to_, weight_)])
+    WEIGHTED = args.weighted
+    format_ = args.format
+    index_ = args.indexed
+    directed_ = args.directed
+    indexed_ = args.index_transform
+
+    # 有向
+    if directed_ == True:
+        m_graph = nx.MultiDiGraph()
+    # 無向
+    else:
+        m_graph = nx.MultiGraph()
+
+    # ファイル指定
+    if problem_file != None:
+        # 頂点の数を取得
+        with open(problem_file) as f:
+            nodes = int(f.readline().split(' ')[0])
+            
+            for i in range(nodes - 1):
+                edge_info = f.readline().split(' ')
+                # 始点、終点、重み
+                from_ = int(edge_info[0])
+                to_ = int(edge_info[1])
+
+                # 重みあり
+                if WEIGHTED == True:
+                    weight_ = int(edge_info[2])
+                    # 辺を追加
+                    m_graph.add_edges_from([(from_, to_, {"label": weight_})])
+                else:
+                    m_graph.add_edges_from([(from_, to_)])
+    # ファイル指定なしの場合は標準入力に従う
+    else:
+        # マトリックス
+        if format_ == 'matrix':
+            print('matrix')
         else:
-            mdgraph.add_edges_from([(from_, to_)])
+            print('edges')
 
-    graph_draw(mdgraph)
+        exit()
+        # 未対応です
+        '''
+        for i in range(num):
+            edge_info = input().split(" ")
+            from_ = int(edge_info[0])
+            to_ = int(edge_info[1])
+            weight_ = 0
+
+            # 入力の確認
+            if len(edge_info) != 3 and len(edge_info) != 2:
+                print("Args Error")
+                sys.exit()
+            elif len(edge_info) == 3 :
+                weight_ = int(edge_info[2])
+
+            # 重み付きの辺を追加
+            if weight_ != 0:
+                mdgraph.add_weighted_edges_from([(from_, to_, weight_)])
+            else:
+                mdgraph.add_edges_from([(from_, to_)])
+        '''
+
+    # グラフ描画
+    graph_draw(m_graph)
