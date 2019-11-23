5c20053eccf636e998ce7364ee5f6e580df5ebe6
mzm <mozumikuon@mzm.local>
2019-11-23 23:38:58 +0900

    issueの#2〜4に一通り対応しました。

--- graph_draw.py
+++ graph_draw.py
@@ -14,7 +14,6 @@ WEIGHTED = False
 # グラフ描画
 def graph_draw(graph: nx.MultiDiGraph):
     pos = nx.spring_layout(graph)
-
     nx.draw_networkx(graph, pos)
 
     ag = nx.nx_agraph.to_agraph(graph)
@@ -41,7 +40,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     problem_file = args.file
-
     WEIGHTED = args.weighted
     format_ = args.format
     directed_ = args.directed
@@ -56,40 +54,86 @@ if __name__ == '__main__':
 
     # ファイル指定
     if problem_file != None:
-        # 頂点の数を取得
         with open(problem_file) as f:
-            nodes = int(f.readline().split(' ')[0])
-            
-            for i in range(nodes - 1):
-                edge_info = f.readline().split(' ')
-                edges = [int(e_i) for e_i in edge_info]
+            file_info = f.readlines()
 
-                # 始点、終点
-                from_ = edges[0]
-                to_ = edges[1]
+        # 頂点数
+        count = file_info[0]
+        node_count = int(count.split(' ')[0])
 
-                # 重みあり
-                if WEIGHTED == True:
-                    weight_ = edges[2]
-                    # 辺を追加
-                    m_graph.add_edges_from([(from_, to_, {"label": weight_})])
-                else:
-                    m_graph.add_edges_from([(from_, to_)])
+        # 枝リスト取得
+        for i in range(1, node_count):
+            edges = file_info[i].split(' ')
+            # 変換ありの始点、終点
+            if indexed_ == True:
+                from_ = int(edges[0]) - 1
+                to_ = int(edges[1]) - 1
+            else:
+                from_ = int(edges[0])
+                to_ = int(edges[1])
+
+            m_graph.add_edges_from([(from_, to_)])
     # ファイル指定なしの場合は標準入力に従う
     else:
         # マトリックス
         if format_ == 'matrix':
-            print('matrix')
-            exit()
+            # 頂点数
+            node_count = int(input())
+
+            # 隣接行列の設定
+            matrix = [[0 for i in range(node_count)] for j in range(node_count)]
+            for i in range(node_count):
+                edge_info = input().split(' ')
+                # 入力確認
+                if len(edge_info) != node_count:
+                    print('input error')
+                    exit()
+                
+                for j in range(node_count):
+                    matrix[i][j] = int(edge_info[j])
+
+            # 隣接行列の解析
+            for i in range(node_count):
+                for j in range(node_count):
+                    # 隣接している
+                    if matrix[i][j] != 0:
+                        # 変換ありの始点、終点
+                        if indexed_ == True:
+                            from_ = i
+                            to_ = j
+                        else:
+                            from_ = i + 1
+                            to_ = j + 1
+
+                        # 重みあり
+                        if WEIGHTED == True:
+                            weight_ = matrix[i][j]
+                            m_graph.add_edges_from([(from_, to_, {'label': weight_})])
+                        else:
+                            m_graph.add_edges_from([(from_, to_)])
         # 枝リスト
         else:
-            for i in range(int(input())):
+            # 頂点数と辺数
+            node_edge = input()
+            node_count = int(node_edge.split(' ')[0])
+            edge_count = int(node_edge.split(' ')[1])
+
+            for i in range(edge_count):
                 edge_info = input().split(' ')
                 edges = [int(e_i) for e_i in edge_info]
                 
-                # 始点、終点
-                from_ = edges[0]
-                to_ = edges[1]
+                # 変換ありの始点、終点
+                if indexed_ == True:
+                    from_ = edges[0] - 1
+                    to_ = edges[1] - 1
+                else:
+                    from_ = edges[0]
+                    to_ = edges[1]
+
+                # 頂点チェック
+                if (node_count < from_) or (node_count < to_):
+                    print('node error')
+                    exit()
 
                 # 重みあり
                 if WEIGHTED == True:
