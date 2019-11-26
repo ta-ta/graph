5ee04ce18e11c9f01c2c1c5a50771befb23b5fae
mzm <mozumikuon@mzm.local>
2019-11-27 00:34:12 +0900

    ファイルありの行列対応、ノードの形を円にしました。

--- graph_draw.py
+++ graph_draw.py
@@ -17,6 +17,7 @@ def graph_draw(graph: nx.MultiDiGraph):
     nx.draw_networkx(graph, pos)
 
     ag = nx.nx_agraph.to_agraph(graph)
+    ag.node_attr['shape'] = 'circle'
     ag.layout()
     ag.draw("graph.png")
 
@@ -28,6 +29,36 @@ def graph_draw(graph: nx.MultiDiGraph):
     plt.tight_layout()
     plt.axis("off")
     plt.show()
+
+# 行列の解析
+def matrix_analyze(count: int, mat: list, graph: nx.MultiDiGraph):
+    for i in range(count):
+        for j in range(count):
+            # 始点、終点
+            from_ = 0
+            to_ = 0
+
+            # 隣接していない場合は処理なし
+            if mat[i][j] != 0:
+                from_ = i + 1
+                to_ = j + 1
+            else:
+                continue
+
+            # 変換有無
+            if indexed_ == True:
+                # 変換あり
+                from_ = i
+                to_ = j
+                
+            # 重み有無
+            if WEIGHTED == True:
+                # 重みあり
+                weight_ = mat[i][j]
+                graph.add_edges_from([(from_, to_, {'label': weight_})])
+            else:
+                # 重みなし
+                graph.add_edges_from([(from_, to_)])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -45,46 +76,32 @@ if __name__ == '__main__':
     directed_ = args.directed
     indexed_ = args.index_transform
 
-    # 有向
+    # 有向/無向の判定
     if directed_ == True:
+        # 有向
         m_graph = nx.MultiDiGraph()
-    # 無向
     else:
+        # 無向
         m_graph = nx.MultiGraph()
 
-    # ファイル指定
+    # ファイル指定の有無
     if problem_file != None:
+        # ファイル指定あり
         with open(problem_file) as f:
             file_info = f.readlines()
-
-        # 頂点数
         count = file_info[0]
-        node_count = int(count.split(' ')[0])
 
-        # 枝リスト取得
-        for i in range(1, node_count):
-            edges = file_info[i].split(' ')
-            # 変換ありの始点、終点
-            if indexed_ == True:
-                from_ = int(edges[0]) - 1
-                to_ = int(edges[1]) - 1
-            else:
-                from_ = int(edges[0])
-                to_ = int(edges[1])
-
-            m_graph.add_edges_from([(from_, to_)])
-    # ファイル指定なしの場合は標準入力に従う
-    else:
-        # マトリックス
-        if format_ == 'matrix':
-            # 頂点数
-            node_count = int(input())
+        # 形式の判定
+        if len(count.split(' ')) == 1:
+            # マトリックス
+            node_count = int(count.split(' ')[0])
 
             # 隣接行列の設定
             matrix = [[0 for i in range(node_count)] for j in range(node_count)]
             for i in range(node_count):
-                edge_info = input().split(' ')
-                # 入力確認
+                edge_info = file_info[i + 1].split(' ')
+
+                # 要素が不足していないかどうか
                 if len(edge_info) != node_count:
                     print('input error')
                     exit()
@@ -92,28 +109,49 @@ if __name__ == '__main__':
                 for j in range(node_count):
                     matrix[i][j] = int(edge_info[j])
 
-            # 隣接行列の解析
-            for i in range(node_count):
-                for j in range(node_count):
-                    # 隣接している
-                    if matrix[i][j] != 0:
-                        # 変換ありの始点、終点
-                        if indexed_ == True:
-                            from_ = i
-                            to_ = j
-                        else:
-                            from_ = i + 1
-                            to_ = j + 1
-
-                        # 重みあり
-                        if WEIGHTED == True:
-                            weight_ = matrix[i][j]
-                            m_graph.add_edges_from([(from_, to_, {'label': weight_})])
-                        else:
-                            m_graph.add_edges_from([(from_, to_)])
-        # 枝リスト
+            # 行列の解析
+            matrix_analyze(node_count, matrix, m_graph)
         else:
-            # 頂点数と辺数
+            # 枝リスト
+            node_count = int(count.split(' ')[0])
+
+            # 枝リスト取得
+            for i in range(1, node_count):
+                edges = file_info[i].split(' ')
+
+                # 変換有無
+                if indexed_ == True:
+                    # 変換あり
+                    from_ = int(edges[0]) - 1
+                    to_ = int(edges[1]) - 1
+                else:
+                    # 変換なし
+                    from_ = int(edges[0])
+                    to_ = int(edges[1])
+                m_graph.add_edges_from([(from_, to_)])
+    else:
+        # 標準入力の指定
+        if format_ == 'matrix':
+            # マトリックス
+            node_count = int(input())
+
+            # 隣接行列の設定
+            matrix = [[0 for i in range(node_count)] for j in range(node_count)]
+            for i in range(node_count):
+                edge_info = input().split(' ')
+
+                # 要素が不足していないかどうか
+                if len(edge_info) != node_count:
+                    print('input error')
+                    exit()
+                
+                for j in range(node_count):
+                    matrix[i][j] = int(edge_info[j])
+
+            # 行列の解析
+            matrix_analyze(node_count, matrix, m_graph)
+        else:
+            # 枝リスト
             node_edge = input()
             node_count = int(node_edge.split(' ')[0])
             edge_count = int(node_edge.split(' ')[1])
@@ -122,11 +160,13 @@ if __name__ == '__main__':
                 edge_info = input().split(' ')
                 edges = [int(e_i) for e_i in edge_info]
                 
-                # 変換ありの始点、終点
+                # 変換有無
                 if indexed_ == True:
+                    # 変換あり
                     from_ = edges[0] - 1
                     to_ = edges[1] - 1
                 else:
+                    # 変換なし
                     from_ = edges[0]
                     to_ = edges[1]
 
@@ -135,11 +175,13 @@ if __name__ == '__main__':
                     print('node error')
                     exit()
 
-                # 重みあり
+                # 重み有無
                 if WEIGHTED == True:
+                    # 重みあり
                     weight_ = edges[2]
                     m_graph.add_edges_from([(from_, to_, {'label': weight_})])
                 else:
+                    # 重みなし
                     m_graph.add_edges_from([(from_, to_)])
 
     # グラフ描画
